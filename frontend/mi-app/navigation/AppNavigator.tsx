c77469b89493aff3c2b1bc09c1ec263c1c743087
LluviaC22 <lluvia.cisneros.isw@unipolidgo.edu.mx>
2025-10-31 15:32:42 -0600

    Conexión al backend en AppNavigator

--- frontend/mi-app/navigation/AppNavigator.tsx
+++ frontend/mi-app/navigation/AppNavigator.tsx
@@ -6,6 +6,9 @@ import LoginScreen from "../screens/LoginScreen";
 import LoginFormScreen from "../screens/LoginFormScreen";
 import RegisterScreen from "../screens/RegisterScreen";
 import ResumenScreen from "../screens/ResumenScreen";
+import RecuperarContrasena from "../screens/RecuperarContrasenaScreen";
+
+
 
 export type RootStackParamList = {
   Splash: undefined;
@@ -13,6 +16,7 @@ export type RootStackParamList = {
   LoginForm: undefined;
   Register: undefined;
   Resumen: undefined;
+   RecuperarContrasena: undefined;
 };
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
@@ -29,6 +33,7 @@ export default function AppNavigator() {
         <Stack.Screen name="LoginForm" component={LoginFormScreen} />
         <Stack.Screen name="Register" component={RegisterScreen} />
         <Stack.Screen name="Resumen" component={ResumenScreen} />
+         <Stack.Screen name="RecuperarContrasena" component={RecuperarContrasena} />
       </Stack.Navigator>
     </NavigationContainer>
   );
